e52df90f04cc21ac4825c35e50a9f6db9157636e
Andrzej <zapala333@gmail.com>
2017-05-15 14:46:57 +0200

    Stworzenie Klas

---
+++ src/dzikizachod/Akcja.java
@@ -0,0 +1,12 @@
+package dzikizachod;
+
+/**
+ * Created by Andrzej on 15.05.2017.
+ */
+public enum Akcja {
+    ULECZ,
+    STRZEL,
+    ZASIEG_PLUS_JEDEN,
+    ZASIEG_PLUS_DWA,
+    DYNAMIT,
+}
---
+++ src/dzikizachod/StrategiaPomocnikaSzeryfaZliczajaca.java
@@ -0,0 +1,7 @@
+package dzikizachod;
+
+/**
+ * Created by Andrzej on 15.05.2017.
+ */
+public class StrategiaPomocnikaSzeryfaZliczajaca extends StrategiaPomocnikaSzeryfa {
+}
